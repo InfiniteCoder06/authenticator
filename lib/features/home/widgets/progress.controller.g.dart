0e71e0beb5a0b00d80cadcf60fb1fcf4a8af034d
Praveen Kumar <68219142+InfiniteCoder06@users.noreply.github.com>
2023-10-21 09:07:00 +0000

    ⬇️ Fix Riverpod Generator Dependency
    Signed-off-by: Praveen Kumar <mallidipraveenkumarreddy@gmail.com>

--- lib/features/home/widgets/progress.controller.g.dart
+++ lib/features/home/widgets/progress.controller.g.dart
@@ -19,5 +19,6 @@ final progressProvider = NotifierProvider<Progress, double>.internal(
   allTransitiveDependencies: null,
 );
 
+typedef _$Progress = Notifier<double>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
