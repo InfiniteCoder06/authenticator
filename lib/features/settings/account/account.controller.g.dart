0d3bddd05fb11112474ace70356e68e4abe4e73c
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2024-02-04 19:01:39 +0530

    📦 Run Build Runner

--- lib/features/settings/account/account.controller.g.dart
+++ lib/features/settings/account/account.controller.g.dart
@@ -6,7 +6,7 @@ part of 'account.controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$accountControllerHash() => r'4eec380d5221a0be6623cae8896220b92bd76d4c';
+String _$accountControllerHash() => r'631738037e669f011640a3852f7e861097972301';
 
 /// See also [AccountController].
 @ProviderFor(AccountController)
@@ -23,4 +23,4 @@ final accountControllerProvider =
 
 typedef _$AccountController = AutoDisposeNotifier<AccountState>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
