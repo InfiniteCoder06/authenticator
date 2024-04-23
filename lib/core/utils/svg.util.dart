d9dd59c537016802dda3aa57f4566baa21abcbaf
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2024-04-23 14:08:20 +0530

    ⚡️ SVG Theming
    Signed-off-by: Praveen Kumar <mallidipraveenkumarreddy@gmail.com>

--- lib/core/utils/svg.util.dart
+++ lib/core/utils/svg.util.dart
@@ -6,8 +6,7 @@ class SvgUtils {
       AssetBundle assetBundle, Color color, String path) async {
     final svgString = await assetBundle.loadString(path);
 
-    String valueString = color.toString().split('(0x')[1].split(')')[0];
-    valueString = valueString.substring(2, valueString.length);
-    return svgString.replaceAll("#6c63ff", "#$valueString");
+    return svgString.replaceAll(
+        "#6c63ff", '#${color.value.toRadixString(16).substring(2)}');
   }
 }
