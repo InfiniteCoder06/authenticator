7957206dddd434d5398b7a45dad3c6eeb269ceff
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2024-04-27 21:12:07 +0530

    ⚡️ Reduce Rebuilds
    Signed-off-by: Praveen Kumar <mallidipraveenkumarreddy@gmail.com>

--- lib/features/home/home.screen.dart
+++ lib/features/home/home.screen.dart
@@ -8,24 +8,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:swipeable_page_route/swipeable_page_route.dart';
 
 // 🌎 Project imports:
 import 'package:authenticator/core/router/app.router.dart';
 import 'package:authenticator/core/utils/constants/config.constant.dart';
 import 'package:authenticator/core/utils/dialog.util.dart';
-import 'package:authenticator/core/utils/local_auth/app_local_auth.widget.dart';
 import 'package:authenticator/core/utils/otp.util.dart';
 import 'package:authenticator/core/utils/qr.util.dart';
 import 'package:authenticator/features/home/home.controller.dart';
+import 'package:authenticator/features/home/widgets/home.appbar.dart';
 import 'package:authenticator/features/home/widgets/home.bottom_sheet.dart';
 import 'package:authenticator/features/home/widgets/progress.widget.dart';
-import 'package:authenticator/features/settings/security/security.controller.dart';
 import 'package:authenticator/gen/assets.gen.dart';
 import 'package:authenticator/modules.dart';
-import 'package:authenticator/widgets/app_bar_title.dart';
 import 'package:authenticator/widgets/app_cross_fade.dart';
-import 'package:authenticator/widgets/app_pop_button.dart';
 import 'package:authenticator/widgets/item/item.body.widget.dart';
 import 'package:authenticator/widgets/item/item.card.widget.dart';
 import 'package:authenticator/widgets/item/item.header.widget.dart';
@@ -51,132 +47,25 @@ class EntryOverviewPage extends HookConsumerWidget {
       return null;
     }, []);
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: Builder(
-          builder: (context) {
-            final selected = ref.watch(selectedEntriesProvider);
-            final hasLock = ref.watch(
-                securityControllerProvider.select((state) => state.isEnabled));
-            return MorphingAppBar(
-              leading: selected.isNotEmpty
-                  ? AppPopButton(
-                      forceCloseButton: true,
-                      tooltip: "Clear",
-                      onPressed: ref
-                          .read(selectedEntriesProvider.notifier)
-                          .clearSelected,
-                    )
-                  : null,
-              title: const AppBarTitle(fallbackRouter: AppRouter.home),
-              actions: [
-                AppCrossFade(
-                  showFirst: selected.isEmpty || selected.length != 1,
-                  firstChild: const SizedBox(height: 48),
-                  secondChild: IconButton(
-                    onPressed: () async {
-                      ref.read(showSearchProvider.notifier).state = false;
-                      await Navigator.of(context).pushNamed(
-                          AppRouter.details.path,
-                          arguments: DetailPageArgs(item: selected.first));
-                      ref
-                          .read(selectedEntriesProvider.notifier)
-                          .clearSelected();
-                      ref.invalidate(getAllItemProvider);
-                    },
-                    icon: const Icon(Icons.edit_rounded),
-                    tooltip: "Edit",
-                  ),
-                ),
-                AppCrossFade(
-                  showFirst: selected.isEmpty,
-                  firstChild: IconButton(
-                    onPressed: () async {
-                      ref.read(showSearchProvider.notifier).state = true;
-                    },
-                    icon: const Icon(Icons.search_rounded),
-                    tooltip: "Search",
-                  ),
-                  secondChild: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () async {
-                          ref.read(showSearchProvider.notifier).state = false;
-                          await Navigator.of(context)
-                              .pushNamed(AppRouter.transfer.path,
-                                  arguments: TransferPageArgs(
-                                    items: selected,
-                                  ));
-                          ref
-                              .read(selectedEntriesProvider.notifier)
-                              .clearSelected();
-                        },
-                        icon: const Icon(Icons.share_rounded),
-                        tooltip: "Transfer",
-                      ),
-                      IconButton(
-                        onPressed: () async {
-                          FocusManager.instance.primaryFocus?.unfocus();
-                          bool hasDeleted = await AppDialogs.showDeletionDialog(
-                              context, selected, ref);
-                          if (hasDeleted) {
-                            ref
-                                .read(selectedEntriesProvider.notifier)
-                                .clearSelected();
-                            ref.invalidate(getAllItemProvider);
-                          }
-                        },
-                        icon: const Icon(Icons.delete_rounded),
-                        tooltip: "Delete",
-                      ),
-                    ],
-                  ),
-                ),
-                PopupMenuButton<int>(
-                  tooltip: "More",
-                  icon: const Icon(Icons.more_vert_rounded),
-                  itemBuilder: (context) => [
-                    const PopupMenuItem(
-                      value: 0,
-                      child: Text("Settings"),
-                    ),
-                    if (hasLock)
-                      const PopupMenuItem(
-                        value: 1,
-                        child: Text("Lock"),
-                      )
-                  ],
-                  onSelected: (value) async {
-                    if (value == 0) {
-                      ref.read(showSearchProvider.notifier).state = false;
-                      await Navigator.of(context)
-                          .pushNamed(AppRouter.settings.path);
-                      ref.invalidate(getAllItemProvider);
-                    }
-                    if (value == 1) {
-                      if (!context.mounted) return;
-                      AppLocalAuth.of(context)?.showLock();
-                    }
-                  },
-                ),
-              ],
-            );
-          },
-        ),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: HomeAppBar(),
       ),
-      body: Builder(
-        builder: (context) {
-          final showSearch = ref.watch(showSearchProvider);
-          final entries =
-              ref.watch(filteredItemsProvider(searchController.text));
-          return Column(
-            children: [
-              AnimatedOpacity(
-                duration: Durations.short4,
-                opacity: entries.isEmpty ? 0 : 1,
-                child: const ProgressBar(),
-              ),
-              AppCrossFade(
+      body: Column(
+        children: [
+          Builder(builder: (context) {
+            final isEmpty =
+                ref.watch(entriesProvider.select((list) => list.isEmpty));
+            return AnimatedOpacity(
+              duration: Durations.short4,
+              opacity: isEmpty ? 0 : 1,
+              child: const ProgressBar(),
+            );
+          }),
+          Builder(
+            builder: (context) {
+              final showSearch = ref.watch(showSearchProvider);
+              return AppCrossFade(
                 duration: Durations.medium2,
                 showFirst: !showSearch,
                 curve: Curves.fastOutSlowIn,
@@ -198,8 +87,14 @@ class EntryOverviewPage extends HookConsumerWidget {
                     ],
                   ),
                 ),
-              ),
-              Expanded(
+              );
+            },
+          ),
+          Builder(
+            builder: (context) {
+              final entries =
+                  ref.watch(filteredItemsProvider(searchController.text));
+              return Expanded(
                 child: entries.isEmpty
                     ? SvgLoader(svgPath: Assets.empty.path)
                     : ListView.builder(
@@ -264,10 +159,10 @@ class EntryOverviewPage extends HookConsumerWidget {
                           );
                         },
                       ),
-              ),
-            ],
-          );
-        },
+              );
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: "Add",
@@ -296,17 +191,13 @@ class EntryOverviewPage extends HookConsumerWidget {
                     .pickImage(source: ImageSource.gallery);
 
                 if (pickedFile == null) {
-                  if (context.mounted) {
-                    AppDialogs.showErrorDialog(
-                        context, "User Cancelled Picker");
-                  }
+                  if (!context.mounted) return;
+                  AppDialogs.showErrorDialog(context, "User Cancelled Picker");
                   return;
                 }
 
                 final data = await pickedFile.readAsBytes();
-
                 final result = await QrUtils().decodeFile(data);
-
                 if (result.isNone() && context.mounted) {
                   AppDialogs.showErrorDialog(context, "QR not Found");
                   return;
---
+++ lib/features/home/widgets/home.appbar.dart
@@ -0,0 +1,120 @@
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+
+// 📦 Package imports:
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:swipeable_page_route/swipeable_page_route.dart';
+
+// 🌎 Project imports:
+import 'package:authenticator/core/router/app.router.dart';
+import 'package:authenticator/core/utils/dialog.util.dart';
+import 'package:authenticator/core/utils/local_auth/app_local_auth.widget.dart';
+import 'package:authenticator/features/home/home.controller.dart';
+import 'package:authenticator/features/settings/security/security.controller.dart';
+import 'package:authenticator/widgets/app_bar_title.dart';
+import 'package:authenticator/widgets/app_cross_fade.dart';
+import 'package:authenticator/widgets/app_pop_button.dart';
+
+class HomeAppBar extends HookConsumerWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selected = ref.watch(selectedEntriesProvider);
+    final hasLock = ref
+        .watch(securityControllerProvider.select((state) => state.isEnabled));
+    return MorphingAppBar(
+      leading: selected.isNotEmpty
+          ? AppPopButton(
+              forceCloseButton: true,
+              tooltip: "Clear",
+              onPressed:
+                  ref.read(selectedEntriesProvider.notifier).clearSelected,
+            )
+          : null,
+      title: const AppBarTitle(fallbackRouter: AppRouter.home),
+      actions: [
+        AppCrossFade(
+          showFirst: selected.isEmpty || selected.length != 1,
+          firstChild: const SizedBox(height: 48),
+          secondChild: IconButton(
+            onPressed: () async {
+              ref.read(showSearchProvider.notifier).state = false;
+              await Navigator.of(context).pushNamed(AppRouter.details.path,
+                  arguments: DetailPageArgs(item: selected.first));
+              ref.read(selectedEntriesProvider.notifier).clearSelected();
+              ref.invalidate(getAllItemProvider);
+            },
+            icon: const Icon(Icons.edit_rounded),
+            tooltip: "Edit",
+          ),
+        ),
+        AppCrossFade(
+          showFirst: selected.isEmpty,
+          firstChild: IconButton(
+            onPressed: () async {
+              ref.read(showSearchProvider.notifier).state = true;
+            },
+            icon: const Icon(Icons.search_rounded),
+            tooltip: "Search",
+          ),
+          secondChild: Row(
+            children: [
+              IconButton(
+                onPressed: () async {
+                  ref.read(showSearchProvider.notifier).state = false;
+                  await Navigator.of(context).pushNamed(AppRouter.transfer.path,
+                      arguments: TransferPageArgs(
+                        items: selected,
+                      ));
+                  ref.read(selectedEntriesProvider.notifier).clearSelected();
+                },
+                icon: const Icon(Icons.share_rounded),
+                tooltip: "Transfer",
+              ),
+              IconButton(
+                onPressed: () async {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  bool hasDeleted = await AppDialogs.showDeletionDialog(
+                      context, selected, ref);
+                  if (hasDeleted) {
+                    ref.read(selectedEntriesProvider.notifier).clearSelected();
+                    ref.invalidate(getAllItemProvider);
+                  }
+                },
+                icon: const Icon(Icons.delete_rounded),
+                tooltip: "Delete",
+              ),
+            ],
+          ),
+        ),
+        PopupMenuButton<int>(
+          tooltip: "More",
+          icon: const Icon(Icons.more_vert_rounded),
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: 0,
+              child: Text("Settings"),
+            ),
+            if (hasLock)
+              const PopupMenuItem(
+                value: 1,
+                child: Text("Lock"),
+              )
+          ],
+          onSelected: (value) async {
+            if (value == 0) {
+              ref.read(showSearchProvider.notifier).state = false;
+              await Navigator.of(context).pushNamed(AppRouter.settings.path);
+              ref.invalidate(getAllItemProvider);
+            }
+            if (value == 1) {
+              if (!context.mounted) return;
+              AppLocalAuth.of(context)?.showLock();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
