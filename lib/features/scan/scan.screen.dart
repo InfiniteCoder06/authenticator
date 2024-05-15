af15b45d9d9b5aa2efa5c4a29eebf5140cddd9d5
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2024-05-15 15:24:12 +0530

    ⚡️ Improve Performance
    Signed-off-by: Praveen Kumar <mallidipraveenkumarreddy@gmail.com>

--- lib/features/scan/scan.screen.dart
+++ lib/features/scan/scan.screen.dart
@@ -32,68 +32,56 @@ class _ScanPageState extends State<ScanPage> {
       detectionSpeed: DetectionSpeed.noDuplicates,
       formats: [BarcodeFormat.qrCode],
       detectionTimeoutMs: 1000,
-    )..start();
-
-    controller.barcodes.listen((barcode) async {
-      controller.stop();
-      final List<Barcode> barcodes = barcode.barcodes;
-
-      if (barcodes.first.rawValue != null) {
-        final result = OtpUtils.parseURI(Uri.parse(barcodes.first.rawValue!));
-        result.match((error) async {
-          await AppDialogs.showErrorDialog(context, error);
-          controller.start();
-        },
-            (item) => Navigator.of(context).pushReplacementNamed(
-                AppRouter.details.path,
-                arguments: DetailPageArgs(item: item, isUrl: true)));
-      } else {
-        if (!mounted) return;
-        await AppDialogs.showErrorDialog(context, 'Unknown');
-        controller.start();
-      }
-    });
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final torchEnabled = useValueNotifier(controller.torchEnabled);
+    final cameraFacing = useValueNotifier(controller.facing);
     return Scaffold(
       appBar: MorphingAppBar(
         title: const AppBarTitle(fallbackRouter: AppRouter.scan),
         actions: [
-          StatefulBuilder(builder: (context, setState) {
-            final torch = controller.torchEnabled;
-            return IconButton(
-              icon: AppCrossFade(
-                showFirst: !torch,
-                firstChild: const Icon(Icons.flash_off_rounded),
-                secondChild:
-                    const Icon(Icons.flash_on_rounded, color: Colors.yellow),
-              ),
-              onPressed: () {
-                controller.toggleTorch();
-                setState(() {});
-              },
-            );
-          }),
-          StatefulBuilder(builder: (context, setState) {
-            final facing = controller.facing;
-            return IconButton(
-              icon: AppCrossFade(
-                showFirst: facing == CameraFacing.front,
-                firstChild: const Icon(Icons.camera_front_rounded),
-                secondChild: const Icon(Icons.camera_rear_rounded),
-              ),
-              onPressed: () {
-                controller.switchCamera();
-                setState(() {});
-              },
-            );
-          }),
+          ValueListenableBuilder(
+            valueListenable: torchEnabled,
+            builder: (context, torch, child) {
+              return IconButton(
+                icon: AppCrossFade(
+                  showFirst: !torch,
+                  firstChild: const Icon(Icons.flash_off_rounded),
+                  secondChild:
+                      const Icon(Icons.flash_on_rounded, color: Colors.yellow),
+                ),
+                onPressed: () {
+                  controller.toggleTorch();
+                  setState(() {});
+                },
+              );
+            },
+          ),
+          ValueListenableBuilder(
+            valueListenable: cameraFacing,
+            builder: (context, cameraFacing, child) {
+              return IconButton(
+                icon: AppCrossFade(
+                  showFirst: cameraFacing == CameraFacing.front,
+                  firstChild: const Icon(Icons.camera_front_rounded),
+                  secondChild: const Icon(Icons.camera_rear_rounded),
+                ),
+                onPressed: () {
+                  controller.switchCamera();
+                  setState(() {});
+                },
+              );
+            },
+          ),
         ],
       ),
       body: MobileScanner(
         controller: controller,
+        onDetect: (barcode) async =>
+            onDetect(context: context, barcodes: barcode.barcodes),
         placeholderBuilder: (_, child) {
           return const Center(
             child: CircularProgressIndicator(),
@@ -114,6 +102,27 @@ class _ScanPageState extends State<ScanPage> {
             const MaterialPreviewOverlay(),
       ),
     );
+  }
+
+  Future<void> onDetect({
+    required BuildContext context,
+    required List<Barcode> barcodes,
+  }) async {
+    controller.stop();
+    if (barcodes.first.rawValue != null) {
+      final result = OtpUtils.parseURI(Uri.parse(barcodes.first.rawValue!));
+      result.match((error) async {
+        await AppDialogs.showErrorDialog(context, error);
+        controller.start();
+      },
+          (item) => Navigator.of(context).pushReplacementNamed(
+              AppRouter.details.path,
+              arguments: DetailPageArgs(item: item, isUrl: true)));
+    } else {
+      if (!mounted) return;
+      await AppDialogs.showErrorDialog(context, 'Unknown');
+      controller.start();
+    }
   }
 
   @override
