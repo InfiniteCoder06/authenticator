3b6db8b013c588ff1519c79f72325552d409097d
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2024-08-25 19:21:06 +0530

    ⚰️ Remove Print Statement
    Signed-off-by: Praveen Kumar <mallidipraveenkumarreddy@gmail.com>

--- lib/core/utils/route_observer.util.dart
+++ lib/core/utils/route_observer.util.dart
@@ -13,17 +13,8 @@ class CustomNavigatorObserver extends NavigatorObserver {
   CustomNavigatorObserver({required this.ref});
 
   @override
-  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    super.didPush(route, previousRoute);
-    print(
-        "Route pushed: ${route.settings.name} from ${previousRoute?.settings.name}");
-  }
-
-  @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPop(route, previousRoute);
-    print(
-        "Route popped: ${previousRoute?.settings.name} from ${route.settings.name}");
     if (route.settings.name == '/details' &&
         previousRoute?.settings.name == '/') {
       ref.refresh(getAllItemProvider);
@@ -42,19 +33,5 @@ class CustomNavigatorObserver extends NavigatorObserver {
       ref.refresh(getAllItemProvider);
       ref.refresh(showBackupNotiProvider);
     }
-  }
-
-  @override
-  void didRemove(Route route, Route? previousRoute) {
-    super.didRemove(route, previousRoute);
-    print(
-        "Route removed: ${route.settings.name} from ${previousRoute?.settings.name}");
-  }
-
-  @override
-  void didReplace({Route? newRoute, Route? oldRoute}) {
-    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
-    print(
-        "Route replaced: ${newRoute?.settings.name} from ${oldRoute?.settings.name}");
   }
 }
